30fedaf1aa7b213268ecf77971f2f16853e2fd3c
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-11-25 19:35:02 +0900

    指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  # before_action : only: [:index, :create]
 
   def index
     @item = Item.includes(:user).order('created_at DESC')
