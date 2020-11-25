bc5bc5a7c588961893e2f3dd2f0a9c0bc96ffd88
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-11-25 15:19:54 +0900

    変数修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,8 +9,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @items = Item.new(item_params)
-    if @items.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
