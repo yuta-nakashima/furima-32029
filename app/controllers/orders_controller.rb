2134972c49025076a6f48386c642ea56e3c05676
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-11-26 17:43:30 +0900

    表示画面の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,34 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
+
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
+
+  #def pay_item
+   # Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    #Payjp::Charge.create(
+     # amount: order_params[:price],  # 商品の値段
+      #card: order_params[:token],    # カードトークン
+      #currency: 'jpy'                 # 通貨の種類（日本円）
+ #   )
+  #end
+
+end
