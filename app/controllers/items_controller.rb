6bf21047f8a667f285f2499c8d52e7018634a8c1
yuta-nakashima <73780934+yuta-nakashima@users.noreply.github.com>
2020-11-30 12:03:30 +0900

    view,controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_params_id, only: [:show, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,16 +20,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
+    @item.destroy
+    redirect_to root_path
+
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :price, :explanation, :category_id, :condition_id, :delivery_from_id, :delivery_fee_id, :delivery_date_id).merge(user_id: current_user.id)
+  end
+
+  def item_params_id
+    @item = Item.find(params[:id])
   end
 
 
